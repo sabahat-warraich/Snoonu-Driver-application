228855013a56c7d295e60e51c79c6460f36c24d2
Muhammad Sabahat <sabahat@snoonu.com>
2022-06-19 15:56:08 +0500

    get task Api Automation

--- src/main/java/app/RestApi/AuthenticateMemberLoginRequest.java
+++ src/main/java/app/RestApi/AuthenticateMemberLoginRequest.java
@@ -8,22 +8,23 @@ import main.java.app.pojo.AuthenticateMemberPojo;
 
 public class AuthenticateMemberLoginRequest {
     String access_token;
-    public String authenticateMember(){
+
+    public String authenticateMember() {
 
         String baseURI = "https://webgateway-staging-lb.falconflex.ai";
         String basePath = "/identity-api/login";
 
         AuthenticateMemberPojo response = RestAssured.given().
-                 baseUri(baseURI)
+                baseUri(baseURI)
                 .basePath(basePath)
-                .formParam("client_id","FalconFlexPortal")
+                .formParam("client_id", "FalconFlexPortal")
                 .formParam("client_secret", "SuperSecretPassword")
-                .formParam("username","sabahat_test@gmail.com")
-                .formParam("password","Sabahat@786")
-                .formParam("grant_type","password")
+                .formParam("username", "sabahat_test@gmail.com")
+                .formParam("password", "Sabahat@786")
+                .formParam("grant_type", "password")
                 .post().as(AuthenticateMemberPojo.class);
 
-        access_token=response.getAccessToken();
+        access_token = response.getAccessToken();
 
         return access_token;
     }
